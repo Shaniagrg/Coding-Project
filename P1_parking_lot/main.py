c850bcbff8cfd0307c4a7944e5ff1e12082ae2d0
Shania Gurung <shaniagurung@Shanias-MacBook-Pro.local>
2025-12-21 23:38:17 -0800

    rearrange code

--- P1_parking_lot/main.py
+++ P1_parking_lot/main.py
@@ -9,7 +9,7 @@ class Level:
         
         self.spots = s
         self.spot_taken:set[int] = set()
-        self.spots_assigned = {}
+        self.spots_assigned = {} #key is vehicle and value is given_spot_to_vehicle
         
     def entry(self):  
         if self.spot_available > 0:
@@ -17,10 +17,6 @@ class Level:
             self.spot_occcupied = self.spot_occcupied + 1
         else:
             print(f"No spot availalabe in level {self.level_number}")
-            
-    def exit(self):
-        self.spot_available = self.spot_available + 1
-        self.spot_occcupied = self.spot_occcupied - 1
 
     def park_vehicle(self,vehicle):
             allocate_spot = random.randrange(len(self.spots))
@@ -33,10 +29,9 @@ class Level:
                 else:
                     self.spot_taken.add(allocate_spot)
                     given_spot_to_vehicle = self.spots[allocate_spot]
-                    self.spots_assigned[vehicle] = given_spot_to_vehicle
+                    self.spots_assigned[vehicle] = given_spot_to_vehicle 
                     break
               
-            
-
-    def remove_vehicle(self):
-        print('hello')
+    def exit(self):
+        self.spot_available = self.spot_available + 1
+        self.spot_occcupied = self.spot_occcupied - 1
