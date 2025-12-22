5076b452b7e4ae9f735cfa518c12b304ff124d34
Shania Gurung <shaniagurung@Shanias-MacBook-Pro.local>
2025-12-21 23:32:47 -0800

    create function to assign each vehiecle to a parking spot

--- P1_parking_lot/main.py
+++ P1_parking_lot/main.py
@@ -6,8 +6,10 @@ class Level:
         self.vehicle_type = v
         self.spot_available = s_a  # Tracks the number of spots available in this level
         self.spot_occcupied = 0  
+        
         self.spots = s
-        self.spots_taken = {}
+        self.spot_taken:set[int] = set()
+        self.spots_assigned = {}
         
     def entry(self):  
         if self.spot_available > 0:
@@ -19,3 +21,22 @@ class Level:
     def exit(self):
         self.spot_available = self.spot_available + 1
         self.spot_occcupied = self.spot_occcupied - 1
+
+    def park_vehicle(self,vehicle):
+            allocate_spot = random.randrange(len(self.spots))
+            while True:
+                if len(self.spot_taken) == len(self.spots):
+                    print(f"All spots are occupied in Level {self.level_number}.")
+                    break
+                elif allocate_spot in self.spot_taken:
+                    continue
+                else:
+                    self.spot_taken.add(allocate_spot)
+                    given_spot_to_vehicle = self.spots[allocate_spot]
+                    self.spots_assigned[vehicle] = given_spot_to_vehicle
+                    break
+              
+            
+
+    def remove_vehicle(self):
+        print('hello')
